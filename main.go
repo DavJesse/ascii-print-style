13f1f935c9dd8ca439137538c6f2473750ee2f31
davodhiambo <jesseomolo@yahoo.com>
2024-08-17 00:41:30 +0300

    files: change path to downloaded file.

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", Web.SubmitFormHandler)               // Serve home page
-	http.HandleFunc("/files/art.txt", Web.DownloadArtHandler) // Route to serve the download
+	http.HandleFunc("/files/download-art.txt", Web.DownloadArtHandler) // Route to serve the download
 	http.HandleFunc("/ascii-art", Web.SubmitFormHandler)      // Serve /ascii-art at POST requests
 	log.Fatal(http.ListenAndServe(":8000", nil))              // Defines host port
 }
